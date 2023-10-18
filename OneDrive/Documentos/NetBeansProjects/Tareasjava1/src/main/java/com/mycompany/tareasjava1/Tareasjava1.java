7dccf3052885f39f0ce4fc86fb2d74df05b6aa34
ASUS <ASUS@DESKTOP-R8689IK>
2023-10-18 11:33:50 -0500

    carga inicial

---
+++ OneDrive/Documentos/NetBeansProjects/Tareasjava1/src/main/java/com/mycompany/tareasjava1/Tareasjava1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tareasjava1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tareasjava1 {
+
+    public static void main(String[] args) {
+        System.out.println("bienvenido damian ");
+    }
+}
