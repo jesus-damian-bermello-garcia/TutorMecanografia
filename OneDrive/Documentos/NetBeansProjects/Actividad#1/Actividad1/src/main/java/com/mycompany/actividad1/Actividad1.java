2dbf2865fb27053ba32d52afae478db561d9634d
ASUS <ASUS@DESKTOP-R8689IK>
2023-10-23 12:33:44 -0500

    initial commit

---
+++ OneDrive/Documentos/NetBeansProjects/Actividad#1/Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Actividad1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
