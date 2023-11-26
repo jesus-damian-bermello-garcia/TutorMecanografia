452731e349b82446346b4a4783fbbc173f06ffc8
ASUS <ASUS@DESKTOP-R8689IK>
2023-11-26 16:22:04 -0500

    punto5.creaciondeObjetosEnArraydentroDeClasePrincipal

--- OneDrive/Documentos/NetBeansProjects/ImpactoEcologico/src/main/java/com/mycompany/impactoecologico/ImpactoEcologico.java
+++ OneDrive/Documentos/NetBeansProjects/ImpactoEcologico/src/main/java/com/mycompany/impactoecologico/ImpactoEcologico.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.impactoecologico;
 
+import java.util.ArrayList;
+
 
 
 
@@ -13,11 +15,19 @@ package com.mycompany.impactoecologico;
  */
 public class ImpactoEcologico {
     public static void main(String[] args) {
+     
+        ArrayList<interfazImpactoEcologico> objetos =new ArrayList <>();
+        edificio edificio = new edificio("Edificio A", 200, 10,  2010);
+        objetos.add((interfazImpactoEcologico) edificio);
+        auto auto = new auto("Toyota", "Corolla", 2022, "Gasolina", 50);
+        objetos.add((interfazImpactoEcologico)auto);
+        bicicleta bicicleta = new bicicleta("Trek", "MountainBike", "De montaña");
+        objetos.add((interfazImpactoEcologico) bicicleta);
+            // Iterar a través de la lista e imprimir el impacto ecológico
+        for (interfazImpactoEcologico objeto : objetos) {
+            System.out.println(objeto + "  Impacto ecológico: " + objeto.obtenerImpactoEcologico() + " gCO2");
+        }
     }
-    
- }
 
+}
    
- 
-
- 
